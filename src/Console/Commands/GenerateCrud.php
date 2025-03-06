d783266a35c1d4a0f6076c3b3863012e0aa37a70
Mohamed <mido.b333@gmail.com>
2025-03-06 23:36:32 +0400

    Add all

--- src/Console/Commands/GenerateCrud.php
+++ src/Console/Commands/GenerateCrud.php
@@ -32,11 +32,11 @@ class GenerateCrud extends Command
         $this->createDirectory(dirname($dataTablePath));
 
         // Generate Controller
-        $controllerStub = file_get_contents(__DIR__.'/../../../resources/stubs/controllers/controller.stub');
-        $controllerStub = str_replace(
+        $controllerStub = file_get_contents(__DIR__.'/../../../resources/stubs/controllers/controller.stub');        $controllerStub = str_replace(
         ['{{ModelName}}', '{{modelVariable}}', '{{pluralModel}}'],
         [$modelName, $modelVariable, $pluralModel],
-        $controllerStub);
+        $controllerStub
+    );
         File::put($controllerPath, $controllerStub);
         $this->info("Controller Created: {$controllerPath}");
 
@@ -65,11 +65,72 @@ class GenerateCrud extends Command
         }
         $this->info("Views Created: {$viewPath}");
 
+        // Generate DataTable
+        File::ensureDirectoryExists(base_path('app/DataTables'));
+        $datatableStub = file_get_contents(resource_path('stubs/datatables/datatable.stub'));
+        $datatableStub = str_replace(
+            ['{{ModelName}}', '{{modelVariable}}', '{{pluralModel}}'],
+            [$modelName, $modelVariable, $pluralModel],
+            $datatableStub
+        );
+        File::put($dataTablePath, $datatableStub);
+        $this->info("DataTable Created: {$dataTablePath}");
+
         // Generate Model
-        $modelStub = file_get_contents(__DIR__.'/../../../resources/stubs/models/model.stub');
+        $modelStub = file_get_contents(resource_path('stubs/models/model.stub'));
         $modelStub = str_replace('{{ModelName}}', $modelName, $modelStub);
         File::put($modelPath, $modelStub);
         $this->info("Model Created: {$modelPath}");
+
+        // Generate Migration
+        $migrationStub = file_get_contents(resource_path('stubs/migrations/migration.stub'));
+        $migrationStub = str_replace('{{pluralModel}}', $pluralModel, $migrationStub);
+        File::put($migrationPath, $migrationStub);
+        $this->info("Migration Created: {$migrationPath}");
+
+        $path = base_path('routes/web.php');
+        $fileContent = file_get_contents($path);
+
+// 1. البحث عن مجموعة الـ admin باستخدام تعبير عادي دقيق
+        $pattern = '/Route::prefix\(\'admin\'\)\s*->name\(\'admin\.\'\)\s*->middleware\(\'auth\'\)\s*->group\(function\s*\(\)\s*{([\s\S]*?)}\);/m';
+
+        if (preg_match($pattern, $fileContent, $matches)) {
+            // 2. استخراج المحتوى الداخلي للمجموعة
+            $groupContent = $matches[1];
+
+            // 3. التحقق من وجود الراوت داخل المجموعة فقط
+            $routePattern = "/Route::resource\('{$pluralModel}'/";
+
+            if (preg_match($routePattern, $groupContent)) {
+                $this->error("Route already exists inside admin group!");
+            } else {
+                // 4. إضافة الراوت الجديد داخل المجموعة
+                $updatedGroup = str_replace(
+                    $groupContent,
+                    $groupContent . "\t\tRoute::resource('{$pluralModel}', \\App\\Http\\Controllers\\Admin\\{$modelName}Controller::class);\n",
+                    $matches[0]
+                );
+
+                $newContent = str_replace($matches[0], $updatedGroup, $fileContent);
+                file_put_contents($path, $newContent);
+                $this->info("Route added successfully inside admin group!");
+            }
+        } else {
+            // 5. حالة عدم وجود مجموعة الـ admin: إنشاؤها وإضافة الراوت
+            $adminGroup = <<<EOT
+                Route::prefix('admin')
+                    ->name('admin.')
+                    ->middleware('auth')
+                    ->group(function () {
+                        Route::resource('{$pluralModel}', \\App\\Http\\Controllers\\Admin\\{$modelName}Controller::class);
+                    });
+                EOT;
+
+            $newContent = $fileContent . "\n" . $adminGroup;
+            file_put_contents($path, $newContent);
+            $this->info("Admin group created with new route!");
+        }
+
 
         $this->info("CRUD for {$modelName} generated successfully!");
     }
